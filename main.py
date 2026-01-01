fe29e2beffcb8d85f9771be775c61e3436c07bdb
Hlebets <glib.kobryn@gmail.com>
2026-01-01 23:07:25 +0200

    refactor main.py to return book text and count words

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import os
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
+
+def count_words(file_contents):
+    words = file_contents.split()
+    num_words = len(words)
+    print(f"Found {num_words} total words")
 
 def main():
-    get_book_text("books/frankenstein.txt")
+    count_words(get_book_text("books/frankenstein.txt"))
 
 if __name__ == "__main__":
     main()
